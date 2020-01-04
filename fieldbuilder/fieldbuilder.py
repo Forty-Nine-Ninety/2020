ad5130887340557a4d263b6e2663c34b07f97939
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-01-04 14:21:14 -0800

    Add FieldBuilder classes

---
+++ fieldbuilder/fieldbuilder.py
@@ -0,0 +1,46 @@
+from field_ui import *
+from field import *
+
+def main():
+    command, quit = "", False
+    field = "n/a"
+    while(quit):
+        command = command.split()
+        if command[0] == "quit":
+            quit = True
+        elif command[0] == "help":
+            pass
+        elif command[0] == "create":
+            pass
+        elif command[0] == "delete":
+            pass
+        elif command[0] == "add":
+            pass
+        elif command[0] == "list":
+            pass
+        elif command[0] == "save":
+            pass
+
+def printHelp():
+    pass
+
+def createField():
+    pass
+
+def deleteElement():
+    pass
+
+def addElement():
+    pass
+
+def listElements():
+    pass
+
+def saveField(field):
+    if field == "n/a":
+        print("Field is null.")
+    else:
+        pass
+
+if __name__ == "__main__":
+    main()
