24ea311ac9417a7fdd6230691da5e70dd378f0e1
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-01-04 20:03:04 -0800

    Update Constants import in RobotContainer

--- 2020/src/main/java/frc/robot/RobotContainer.java
+++ 2020/src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,12 @@ import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import static frc.robot.Constants.*;
 
 public class RobotContainer {
 
-    private final JoystickF310 joystickDrive = new JoystickF310(Constants.PORT_JOYSTICK_DRIVE);
-    private final JoystickF310 joystickOperator = new JoystickF310(Constants.PORT_JOYSTICK_OPERATOR);
+    private final JoystickF310 joystickDrive = new JoystickF310(PORT_JOYSTICK_DRIVE);
+    private final JoystickF310 joystickOperator = new JoystickF310(PORT_JOYSTICK_OPERATOR);
 
     private final DrivetrainSubsystem m_drivetrain = new DrivetrainSubsystem();
 
