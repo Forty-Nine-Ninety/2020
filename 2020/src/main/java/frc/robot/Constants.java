d7838fd9ce49970c6f19ab94dce7095d820275c4
gryphonrobotics (DS Computer 1) <gryphonrobotics@csus.org>
2020-01-10 15:15:03 -0800

    Update motor ports for robot on a board

--- 2020/src/main/java/frc/robot/Constants.java
+++ 2020/src/main/java/frc/robot/Constants.java
@@ -5,6 +5,6 @@ public final class Constants {
     public static int PORT_JOYSTICK_DRIVE = 0;
     public static int PORT_JOYSTICK_OPERATOR = 1;
 
-    public static int CAN_DRIVETRAIN_LEFT_TALONSRX = 32;
-    public static int CAN_DRIVETRAIN_RIGHT_TALONSRX = 31;
+    public static int CAN_DRIVETRAIN_LEFT_TALONSRX = 36;
+    public static int CAN_DRIVETRAIN_RIGHT_TALONSRX = 32;
 }
