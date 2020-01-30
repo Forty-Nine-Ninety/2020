477ef12f579d601551b2c7dbe7e51cb82721e804
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-01-29 16:16:42 -0800

    Removing useless stuff

--- 2020/src/main/java/frc/robot/Constants.java
+++ 2020/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import io.github.oblarg.oblog.annotations.*;
 
 public final class Constants {
     public static int PORT_JOYSTICK_DRIVE = 0;
@@ -10,6 +11,8 @@ public final class Constants {
     public static int CAN_DRIVETRAIN_RIGHT_REAR_TALONSRX = 3;
     public static int CAN_DRIVETRAIN_RIGHT_FRONT_TALONSRX = 4;
 
+    //Just a note, this thing is not a constant because you can change it on smartdashboard so we might have to move it to a command if it makes it to final code
+    @Config
     public static double SLOW_MULTIPLIER = 0.3;
 
 }
