d07d70b1750339c57c32a186396c55396fd7cf9b
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-01-30 12:18:49 -0800

    Add new shoot commands

---
+++ 2020/src/main/java/frc/robot/commands/LimelightShootSingleCommand.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+//TODO figure out which kind of command this should be
+public class LimelightShootSingleCommand extends CommandBase {
+
+    public LimelightShootSingleCommand() {
+    }
+}
