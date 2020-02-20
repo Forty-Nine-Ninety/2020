657b8656f697c5ef32e235fcedf19e257b4d0753
Arjun Parthasarathy <aparthasarathy23@csus.org>
2020-02-19 16:37:40 -0800

    created climb-related stuff
    Created ClimbSubsystem, ClimbCommand, and ClimbBalanceCommand

--- 2020/src/main/java/frc/robot/commands/ClimbBalanceCommand.java
+++ 2020/src/main/java/frc/robot/commands/ClimbBalanceCommand.java
@@ -1,20 +1,29 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClimbSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import static frc.robot.Constants.*;
 
 public class ClimbBalanceCommand extends CommandBase {
 
+    private final DrivetrainSubsystem m_drive;
+    private final PIDController m_pid;
     private final ClimbSubsystem m_climb;
 
-    public ClimbBalanceCommand(ClimbSubsystem climb) {
+    public ClimbBalanceCommand(DrivetrainSubsystem drive, ClimbSubsystem climb) {
         addRequirements(climb);
+        m_drive = drive;
         m_climb = climb;
+        m_pid = new PIDController(BALANCE_KP, BALANCE_KI, BALANCE_KD);
     }
 
     @Override
     public void execute() {
-        //TODO
+        double tilt = m_drive.getGyroTilt();
+        double val = m_pid.calculate(tilt, 0.0);
+        m_climb.runBalance(val);
     }
 
 }
--- 2020/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ 2020/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,7 +1,8 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
@@ -9,19 +10,53 @@ import static frc.robot.Constants.*;
 public class ClimbSubsystem extends SubsystemBase {
 
     private final WPI_TalonSRX m_climb, m_balance;
-    private final Solenoid m_lock;
+    private final Solenoid m_solenoid;
 
     public ClimbSubsystem() {
         m_climb = new WPI_TalonSRX(CAN_CLIMB_MAIN_TALONSRX);
         m_balance = new WPI_TalonSRX(CAN_CLIMB_BALANCE_TALONSRX);
-        m_lock = new Solenoid(PCM_CLIMB);
+
+        m_solenoid = new Solenoid(CAN_PCM, PCM_CLIMB);
+		m_solenoid.set(false);
+
+        configureMotors();
     }
 
     @Override
     public void periodic() {
-        //TODO
     }
 
+    public void climb(double position) {
+        m_climb.set(ControlMode.Position, position);
+    }
+    
+    public int getEncoderTicks(){
+        return m_climb.getSelectedSensorPosition();
+    }
 
-    //TODO
+    public void setLock(boolean lock){
+        m_solenoid.set(lock);
+    }
+
+    private void configureMotors() {
+        
+        //First setup talons with default settings
+        m_climb.configFactoryDefault();
+        m_balance.configFactoryDefault();
+
+        //Setup talon built-in PID
+        m_climb.configSelectedFeedbackSensor(TALON_DEFAULT_FEEDBACK_DEVICE, TALON_DEFAULT_PID_ID, TALON_TIMEOUT_MS);
+
+        //Create config objects
+        TalonSRXConfiguration config = new TalonSRXConfiguration();
+
+        //Setup config objects with desired values
+        config.slot0 = CLIMB_FPID;
+
+        //Configure talons
+        m_climb.configAllSettings(config);
+    }
+    public void runBalance(double speed){
+        m_balance.set(speed);
+    }
 }
