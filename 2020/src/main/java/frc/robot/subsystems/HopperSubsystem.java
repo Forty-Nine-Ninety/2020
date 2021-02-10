1811c309db6518107ea05c9e0c38143e36baed03
Dat-Boi-Arjun <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2021-02-09 16:22:19 -0800

    Added second hopper motor

--- 2020/src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ 2020/src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -9,11 +9,13 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class HopperSubsystem extends SubsystemBase implements Loggable {
     
-    private final WPI_TalonSRX m_motor;
+    private final WPI_TalonSRX m_leftMotor;
+    private final WPI_TalonSRX m_rightMotor;
     private final DigitalInput m_ballSensor;
 
     public HopperSubsystem() {
-        m_motor = new WPI_TalonSRX(CAN_LEFT_HOPPER_TALONSRX);
+        m_leftMotor = new WPI_TalonSRX(CAN_LEFT_HOPPER_TALONSRX);
+        m_rightMotor = new WPI_TalonSRX(CAN_RIGHT_HOPPER_TALONSRX);
         m_ballSensor = new DigitalInput(DIO_BREAKBEAM_HOPPER);
     }
 
@@ -21,7 +23,8 @@ public class HopperSubsystem extends SubsystemBase implements Loggable {
     public void periodic() {}
 
     public void set(boolean on) {
-        m_motor.set(on ? HOPPER_MOTOR_SPEED : 0);
+        m_leftMotor.set(on ? HOPPER_MOTOR_SPEED : 0);
+        m_rightMotor.set(on ? -1 * HOPPER_MOTOR_SPEED : 0);
     }
 
     public boolean hasBall() {
