f06c130bd8fae3a1faa22a18f40c7e4d02465c03
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-02-24 16:45:04 -0800

    Update TestBallCommand.java

--- 2020/src/main/java/frc/robot/commands/tests/TestBallCommand.java
+++ 2020/src/main/java/frc/robot/commands/tests/TestBallCommand.java
@@ -1,16 +1,13 @@
 package frc.robot.commands.tests;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class TestBallCommand extends CommandBase{
+public class TestBallCommand extends CommandBase {
 
     private final ShooterSubsystem m_shooter;
     private final HopperSubsystem m_hopper;
-
-    private final double SHOOTER_FIRE_PERCENTAGE = 0.8;
 
     public TestBallCommand(ShooterSubsystem shooter, HopperSubsystem hopper){
         addRequirements(shooter);
@@ -22,16 +19,6 @@ public class TestBallCommand extends CommandBase{
 
     @Override
     public void execute() {
-        m_shooter.setFireSpeed(SHOOTER_FIRE_PERCENTAGE);
-
-        m_hopper.setReverse(true);
-        Timer.delay(0.5);
-        m_hopper.setReverse(false);
-
-        m_shooter.runInserter(SHOOTER_FIRE_PERCENTAGE);
-        m_hopper.set(true);
-
-
     }
 
 
