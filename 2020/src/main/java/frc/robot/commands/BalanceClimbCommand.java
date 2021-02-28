dce3bab5ac732a8c7c027469e222dfbcfa73abb4
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-02-27 18:53:44 -0800

    Update drivetrain commands 2

--- 2020/src/main/java/frc/robot/commands/BalanceClimbCommand.java
+++ 2020/src/main/java/frc/robot/commands/BalanceClimbCommand.java
@@ -8,20 +8,20 @@ import static frc.robot.Constants.*;
 
 public class BalanceClimbCommand extends CommandBase {
 
-    private final DrivetrainSubsystem m_drive;
+    private final DrivetrainSubsystem m_drivetrain;
     private final PIDController m_pid;
     private final ClimbSubsystem m_climb;
 
-    public BalanceClimbCommand(ClimbSubsystem climb, DrivetrainSubsystem drive) {
+    public BalanceClimbCommand(ClimbSubsystem climb, DrivetrainSubsystem drivetrain) {
         addRequirements(climb);
-        m_drive = drive;
+        m_drivetrain = drivetrain;
         m_climb = climb;
         m_pid = new PIDController(CLIMB_BALANCE_KP, CLIMB_BALANCE_KI, CLIMB_BALANCE_KD);
     }
 
     @Override
     public void execute() {
-        double tilt = m_drive.getGyroTilt();
+        double tilt = m_drivetrain.getGyroTilt();
         double val = m_pid.calculate(tilt, 0.0);
         m_climb.runBalance(val);
     }
