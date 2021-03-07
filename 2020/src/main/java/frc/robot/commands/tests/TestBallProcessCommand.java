74e490ad84b5d460f2b4681ea3ebc5b435bcb8d7
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-03-07 15:55:11 -0800

    Refactor ball test command

--- 2020/src/main/java/frc/robot/commands/tests/TestBallProcessCommand.java
+++ 2020/src/main/java/frc/robot/commands/tests/TestBallProcessCommand.java
@@ -1,21 +1,26 @@
-package frc.robot.commands;
+package frc.robot.commands.tests;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Direction;
 import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.StorageSubsystem;
+import frc.robot.commands.*;
 
-public class StorageButtonCommand extends ParallelCommandGroup{
+public class TestBallProcessCommand extends ParallelCommandGroup {
 
     private final HopperSubsystem m_hopper;
+    private final IntakeSubsystem m_intake;
     private final StorageSubsystem m_storage;
 
-    public StorageButtonCommand(HopperSubsystem hopper, StorageSubsystem storage) {
+    public TestBallProcessCommand(HopperSubsystem hopper, IntakeSubsystem intake, StorageSubsystem storage) {
         m_hopper = hopper;
+        m_intake = intake;
         m_storage = storage;
         
         addCommands(
            new RunHopperCommand(m_hopper, Direction.Forward),
+           new RunIntakeCommand(m_intake, Direction.Forward),
            new RunStorageCommand(m_storage, Direction.Forward)
         );
     }
